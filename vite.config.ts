0cea650d6f844cbf61f185e57843d065d9bd64f9
bellDev-code <sumaoo20@naver.com>
2023-10-31 17:34:06 +0900

    absolute path & husky setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@src', replacement: resolve(__dirname, 'src') },
+      {
+        find: '@Components',
+        replacement: resolve(__dirname, 'src/Components'),
+      },
+      {
+        find: '@Layouts',
+        replacement: resolve(__dirname, 'src/Layouts')
+      },
+      {
+        find: '@Pages',
+        replacement: resolve(__dirname, 'src/Pages')
+      }
+    ],
+  },
+  plugins: [react(), tsconfigPaths()]
 })
