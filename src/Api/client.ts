bf8f9b02ad92321b6b5e2947f6a4cb097a3395db
soohan <soohan0808@gmail.com>
2023-11-09 09:42:51 +0900

    ✨ production 살제 서버 url 추가

--- src/Api/client.ts
+++ src/Api/client.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:8888';
+export const BASE_URL =
+  import.meta.env.MODE === 'production'
+    ? 'https://qa-service-i3r6oclqbq-de.a.run.app' //gcr을 이용해 서버를 배포했을 때의 주소
+    : 'http://localhost:8888';
 
 export const client = axios.create({
   baseURL: BASE_URL,
