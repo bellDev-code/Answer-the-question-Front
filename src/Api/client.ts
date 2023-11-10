8e94ca516e3e4f2c1d8a1b96904e165254b0f5fd
soohan <soohan0808@gmail.com>
2023-11-10 10:13:09 +0900

    ✨ api url변경

--- src/Api/client.ts
+++ src/Api/client.ts
@@ -1,9 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL =
-  import.meta.env.MODE === 'production'
-    ? 'https://qa-service-i3r6oclqbq-de.a.run.app' //gcr을 이용해 서버를 배포했을 때의 주소
-    : 'http://localhost:8888';
+export const BASE_URL = 'https://qa-service-i3r6oclqbq-de.a.run.app';
 
 export const client = axios.create({
   baseURL: BASE_URL,
