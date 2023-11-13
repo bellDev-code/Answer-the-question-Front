23d05bc0995b643e9bc2f82603c1039671bd72ed
bellDev-code <sumaoo20@naver.com>
2023-11-13 22:12:02 +0900

    kakao shared button

---
+++ src/Components/KakaoShared/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default function KakaoShare() {
+  const kakaoButton = async () => {
+    if (window.Kakao) {
+      const kakao = window.Kakao;
+
+      if (!kakao.isInitialized()) {
+        kakao.init('da9097fb608a8158e3b3aa48e8585256');
+      }
+
+      kakao.Share.sendDefault({
+        objectType: 'feed',
+        content: {
+          title: '취중진담',
+          description: '질문이 이어지는 곳, 우리의 대화가 깊어지는 순간',
+          imageUrl: 'https://i.imgur.com/Jwy8VuC.png',
+          link: {
+            mobileWebUrl: 'https://answer-the-question-front.vercel.app',
+            webUrl: 'https://answer-the-question-front.vercel.app',
+          },
+        },
+        buttons: [
+          {
+            title: '게임 즐기러 가기!',
+            link: {
+              mobileWebUrl: 'https://answer-the-question-front.vercel.app',
+              webUrl: 'https://answer-the-question-front.vercel.app',
+            },
+          },
+        ],
+      });
+    }
+  };
+
+  return (
+    <button
+      className='sm:text-md bg-yellow-400 py-1 px-3 text-white rounded-md'
+      onClick={kakaoButton}
+    >
+      카카오 공유하기
+    </button>
+  );
+}
--- src/Pages/SelectQApage/index.tsx
+++ src/Pages/SelectQApage/index.tsx
@@ -43,14 +43,14 @@ const SelectQApage = () => {
         <GuideTextComponent>
           {!isRandom && isFirstGame
             ? '첫번째 질문은 처음 작성 된 유저가 선택 됩니다.'
-            : '질문 대상자를 선택한 후 건네기 버튼을 누르고 기기를 질문 대상자에게 주세요'}
+            : '질문 대상자를 선택한 후 건내기 버튼을 누르고 기기를 질문 대상자에게 주세요'}
 
           {isRandom && '답변 후 다음 질문을 위해 다음 사람을 선택 한 후 버튼을 눌러주세요'}
         </GuideTextComponent>
       </div>
 
       <div className='flex justify-end px-10'>
-        <BaseButton onClick={handlePass}>건네기</BaseButton>
+        <BaseButton onClick={handlePass}>건내기</BaseButton>
       </div>
     </PlayGameLayout>
   );
