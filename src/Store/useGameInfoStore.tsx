ac31515e4e64678328543707335b0384e176cabb
bellDev-code <sumaoo20@naver.com>
2023-11-15 00:15:46 +0900

    Multi-create Room

--- src/Store/useGameInfoStore.tsx
+++ src/Store/useGameInfoStore.tsx
@@ -1,16 +1,20 @@
-import { IResponseGameInfo } from '@Api/types';
+import { IResponseGameInfo, IResponseMultiInfo } from '@Api/types';
 import { create } from 'zustand';
 
 interface ApiStore {
   gameInfoResult: IResponseGameInfo | null;
+  multiInfoResult: IResponseMultiInfo | null;
 
   setApiResult: (result: IResponseGameInfo) => void;
+  setMultiResult: (result: IResponseMultiInfo) => void;
 }
 
 const gameInfoStore = create<ApiStore>((set) => ({
   gameInfoResult: null,
+  multiInfoResult: null,
 
   setApiResult: (result) => set({ gameInfoResult: result }),
+  setMultiResult: (result) => set({ multiInfoResult: result }),
 }));
 
 export default gameInfoStore;
