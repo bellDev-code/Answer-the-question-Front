d93e399dfbea8c239ac381fc8829376902c58df4
bellDev-code <sumaoo20@naver.com>
2023-11-17 18:33:47 +0900

    초대 모달창 ui 수정 및 api 연동

--- src/Pages/MultiRoom/component/UsernameModal/index.tsx
+++ src/Pages/MultiRoom/component/UsernameModal/index.tsx
@@ -1,8 +1,10 @@
 import { useJoinRoomQuery } from '@Api/multiGame';
-import { SESSION_USERNAME } from '@Configure/constant';
+import { BaseButton } from '@Components/atom/button/BaseButton';
+import { DYNAMIC_ROUTE_PATH, SESSION_USERNAME } from '@Configure/constant';
+import gameInfoStore from '@Store/useGameInfoStore';
 import { Box, Modal } from '@mui/material';
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 interface IProps {
@@ -14,6 +16,8 @@ interface IProps {
 const index: React.FC<IProps> = ({ isOpen, setIsOpen }) => {
   const { gameId } = useParams();
   const [username, setUsername] = useState('');
+  const navigate = useNavigate();
+  const { setMultiResult } = gameInfoStore();
   const handleChangeUserName = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(e.target.value);
   };
@@ -31,12 +35,17 @@ const index: React.FC<IProps> = ({ isOpen, setIsOpen }) => {
           if (data.code === 200) {
             sessionStorage.setItem(SESSION_USERNAME, username || '');
             setIsOpen(false);
+            setMultiResult(data.data);
+            navigate(`${DYNAMIC_ROUTE_PATH(gameId || '', 0).WATING_PAGE}`);
           }
         },
         onError: (error) => {
           if (error.response?.data.code === 400) {
             if (error.response.data.message === 'Username already exists') {
               toast('이미 존재하는 이름입니다.');
+            }
+            if (error.response?.data.message === 'Game has already started') {
+              toast('게임이 시작되어 입장할 수 없습니다.');
             }
           }
         },
@@ -57,7 +66,7 @@ const index: React.FC<IProps> = ({ isOpen, setIsOpen }) => {
             height: '20%',
             bgcolor: 'background.paper',
             borderRadius: 2,
-            padding: 3,
+            padding: 2,
             position: 'fixed',
             top: '50%',
             left: '50%',
@@ -65,14 +74,18 @@ const index: React.FC<IProps> = ({ isOpen, setIsOpen }) => {
             overflow: 'auto',
           }}
         >
-          <h2>이름을 입력하세요</h2>
-          <input
-            type='text'
-            placeholder='Username'
-            value={username}
-            onChange={handleChangeUserName}
-          />
-          <button onClick={handleSumit}>제출</button>
+          <div className='flex justify-around items-center gap-3'>
+            <h2>이름을 입력하세요</h2>
+            <input
+              type='text'
+              placeholder='Username'
+              value={username}
+              onChange={handleChangeUserName}
+            />
+          </div>
+          <div className='flex items-center justify-center py-3'>
+            <BaseButton onClick={handleSumit}>제출</BaseButton>
+          </div>
         </Box>
       </Modal>
     </div>
